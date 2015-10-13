3d7781672b7fe3071eab4d214370840641193d98
Michal Czaplinski <mmczaplinski@gmail.com>
2015-10-14 00:32:56 +0200

    commit before refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -1,182 +1,172 @@
-$(document).ready(function() {
+var camera, scene, renderer;
+var geometry, material, mesh;
 
-    var camera, scene, renderer;
-    var geometry, material, mesh;
+var objects = [];
+var bullets = [];
 
-    var objects = [];
-    var bullets = [];
+var prevTime = performance.now();
 
-    var prevTime = performance.now();
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
 
-    var raycaster = new THREE.Raycaster();
-    var mouse = new THREE.Vector2();
+camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1000 );
+scene = new THREE.Scene();
+scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
 
-    scene = new THREE.Scene();
-    scene.fog = new THREE.Fog( 0xffffff, 0, 750 );
+var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
+light.position.set( 0.5, 1, 0.75 );
+scene.add( light );
 
-    var light = new THREE.HemisphereLight( 0xeeeeff, 0x777788, 0.75 );
-    light.position.set( 0.5, 1, 0.75 );
-    scene.add( light );
+controls = new THREE.PointerLockControls( camera );
+scene.add( controls.getObject() );
 
-    controls = new THREE.PointerLockControls( camera );
-    scene.add( controls.getObject() );
+// floor
 
-    // floor
+geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
+geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 
-    geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
-    geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
+for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
 
-    for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
+    var vertex = geometry.vertices[ i ];
+    vertex.x += Math.random() * 20 - 10;
+    vertex.y += Math.random() * 2;
+    vertex.z += Math.random() * 20 - 10;
 
-        var vertex = geometry.vertices[ i ];
-        vertex.x += Math.random() * 20 - 10;
-        vertex.y += Math.random() * 2;
-        vertex.z += Math.random() * 20 - 10;
+}
 
-    }
+for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
 
-    for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
+    var face = geometry.faces[ i ];
+    face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+    face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+    face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
 
-        var face = geometry.faces[ i ];
-        face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-        face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-        face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+}
 
-    }
+material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
 
-    material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
+mesh = new THREE.Mesh( geometry, material );
+scene.add( mesh );
 
-    mesh = new THREE.Mesh( geometry, material );
+// objects
+
+geometry = new THREE.BoxGeometry( 20, 20, 20 );
+
+for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
+
+    var face = geometry.faces[ i ];
+    face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+    face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+    face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+
+}
+
+for ( var i = 0; i < 500; i ++ ) {
+
+    material = new THREE.MeshPhongMaterial( { specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors } );
+
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
+    mesh.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
+    mesh.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
     scene.add( mesh );
 
-    // objects
+    material.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
 
-    geometry = new THREE.BoxGeometry( 20, 20, 20 );
+    objects.push( mesh );
 
-    for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
+}
 
-        var face = geometry.faces[ i ];
-        face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-        face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-        face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+//
 
-    }
+renderer = new THREE.WebGLRenderer();
+renderer.setClearColor( 0xffffff );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-    for ( var i = 0; i < 500; i ++ ) {
+//
 
-        material = new THREE.MeshPhongMaterial( { specular: 0xffffff, shading: THREE.FlatShading, vertexColors: THREE.VertexColors } );
+window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener( 'mousemove', onMouseMove, false );
+window.addEventListener( 'click', shootBullet, false );
 
-        var mesh = new THREE.Mesh( geometry, material );
-        mesh.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
-        mesh.position.y = Math.floor( Math.random() * 20 ) * 20 + 10;
-        mesh.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
-        scene.add( mesh );
+animate();
 
-        material.color.setHSL( Math.random() * 0.2 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
+function onWindowResize() {
 
-        objects.push( mesh );
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-    }
-
-    //
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setClearColor( 0xffffff );
-    renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
 
-    //
+}
 
-    window.addEventListener( 'resize', onWindowResize, false );
-    window.addEventListener( 'mousemove', onMouseMove, false );
-    window.addEventListener( 'click', shootBullet, false );
+function onMouseMove( event ) {
 
-    animate();
+    // calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
 
-    function onWindowResize() {
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
+}
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
+function shootBullet ( event ) {
 
-    }
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( {color: 0x222222} );
+    var bullet = new THREE.Mesh( geometry, material );
 
-    function onMouseMove( event ) {
+    scene.add( bullet );
 
-        // calculate mouse position in normalized device coordinates
-        // (-1 to +1) for both components
+    bullet.position.setX(controls.getObject().position.x )
+    bullet.position.setY(controls.getObject().position.y )
+    bullet.position.setZ(controls.getObject().position.z )
 
-        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    bullet.direction = camera.getWorldDirection()
 
-    }
+    bullets.push(bullet);
 
-    function shootBullet ( event ) {
+}
 
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshBasicMaterial( {color: 0x222222} );
-        var bullet = new THREE.Mesh( geometry, material );
+function updateBullets(bullets) {
 
-        scene.add( bullet );
-
-        bullet.position.setX(controls.getObject().position.x )
-        bullet.position.setY(controls.getObject().position.y )
-        bullet.position.setZ(controls.getObject().position.z )
-
-        bullet.direction = camera.getWorldDirection()
-
-        bullets.push(bullet);
-
-    }
-
-    function updateBullets(bullets) {
-
-        bullets.forEach(function(bullet, index, array) {
-            bullet.translateOnAxis(bullet.direction, 10);
-        })
-    }
+    bullets.forEach(function(bullet, index, array) {
+        bullet.translateOnAxis(bullet.direction, 10);
+    })
+}
 
 
-    // function makePlayer(camera) {
-    //
-    // }
+function makePlayer(camera) {
+}
 
-    // var socket = io();
+var socket = io.connect('http://localhost:3000');
 
-    // function UpdateGameData() {
-    //     $('form').submit(function(){
-    //       socket.emit('chat message', $('#m').val());
-    //       $('#m').val('');
-    //       return false;
-    //     });
+function sendState() {
+    socket.emit('gameState', { position: controls.getObject().position });
+}
 
-    //     socket.on('chat message', function(msg){
-    //       $('#messages').append($('<li>').text(msg));
-    //     });
+window.addEventListener( 'click', sendState, false );
 
-    // }
 
-    function animate() {
 
-        requestAnimationFrame( animate );
+function animate() {
 
-        var time = performance.now();
-        var delta = ( time - prevTime ) / 1000;
-        controls.update( objects, delta )
+    requestAnimationFrame( animate );
 
-        prevTime = time;
+    var time = performance.now();
+    var delta = ( time - prevTime ) / 1000;
+    controls.update( objects, delta )
 
-        // update the picking ray with the camera and mouse position
-        raycaster.setFromCamera( mouse, camera );
+    prevTime = time;
 
-        updateBullets(bullets)
+    // update the picking ray with the camera and mouse position
+    raycaster.setFromCamera( mouse, camera );
 
-        renderer.render( scene, camera );
+    updateBullets(bullets)
 
-    }
+    renderer.render( scene, camera );
 
-});
+}
